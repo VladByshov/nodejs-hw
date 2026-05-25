9326ce39fa2c824d799f5f0ada7ace9c557f741b
Byshov <byshov1998@gmail.com>
2026-05-25 21:23:13 +0200

    add global error handler and auth middleware

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,6 +1,7 @@
 import { isHttpError } from 'http-errors';
 
 export const errorHandler = (err, req, res, next) => {
+  console.error('🔥 СПРАВЖНЯ ПОМИЛКА:', err);
   if (isHttpError(err)) {
     return res.status(err.status).json({
       message: err.message,
