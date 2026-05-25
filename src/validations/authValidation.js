816c6fafa2596cf5fae2e6a58019ccf0ab3511ee
Byshov <byshov1998@gmail.com>
2026-05-25 21:24:05 +0200

    implement user auth module (register, login, logout)

---
+++ src/validations/authValidation.js
@@ -0,0 +1,15 @@
+import { celebrate, Segments, Joi } from 'celebrate';
+
+export const registerUserSchema = celebrate({
+  [Segments.BODY]: Joi.object().keys({
+    email: Joi.string().email().required(),
+    password: Joi.string().min(8).required(),
+  }),
+});
+
+export const loginUserSchema = celebrate({
+  [Segments.BODY]: Joi.object().keys({
+    email: Joi.string().email().required(),
+    password: Joi.string().required(),
+  }),
+});
